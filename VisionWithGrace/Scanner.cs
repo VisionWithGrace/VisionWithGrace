5d86f7b24be5524d0761fee37ab55d8f0a32a9ad
Thomas Lovett <tklovett@gmail.com>
2013-11-12 19:16:03 -0500

    abstracted out scanning class

---
+++ VisionWithGrace/Scanner.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VisionWithGrace
+{
+    class Scanner
+    {
+        Timer timer = new Timer();
+        bool isHandlerSet = false;
+
+        public Scanner()
+        {
+            // Initialize timer to simulate 50 Kinect frames
+            // 50 frames/tick * 30 FPS != 1667 ms/tick
+            timer.Interval = 1667;
+            timer.Tick += new EventHandler(on_tick);
+            curObject = 0;
+        }
+
+        public void on_tick(object sender, EventArgs e)
+        {
+            if (numObjects == 0)
+                return;
+
+            curObject = (curObject + 1) % numObjects;
+
+            if (isHandlerSet)
+                onChange.Invoke(sender, e);
+        }
+
+        private EventHandler onChange;
+        public EventHandler OnChange
+        {
+            set
+            {
+                onChange = value;
+                if (value != null)
+                    isHandlerSet = true;
+            }
+        }
+
+        private int numObjects;
+        public int NumObjects
+        {
+            get
+            {
+                return numObjects;
+            }
+            set
+            {
+                numObjects = value;
+                curObject = 0;
+            }
+        }
+
+        private int curObject;
+        public int CurObject
+        {
+            get
+            {
+                return curObject;
+            }
+        }
+
+        public bool isRunning
+        {
+            get
+            {
+                return timer.Enabled;
+            }
+        }
+
+        public void start()
+        {
+            if (timer.Enabled)
+                return;
+
+            timer.Start();
+            onChange(null, new EventArgs());
+        }
+        public void stop()
+        {
+            timer.Stop();
+        }
+    }
+}
